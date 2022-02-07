da586d4c37515fac8da17f08afbf6b56c0c7fb48
Andreea Lakatos <andreealakatos17@yahoo.com>
2022-02-07 17:50:12 +0200

    Project setup

---
+++ src/AnimalAdoption.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AnimalAdoption.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/AnimalAdoption.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AnimalAdoption.Tests
+{
+    public class Class1
+    {
+    }
+}
