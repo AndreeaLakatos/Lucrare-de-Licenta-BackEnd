5045a905ea54756933efd440f23f3bf59f1445c0
Andreea Lakatos <andreealakatos17@yahoo.com>
2022-06-30 11:16:36 +0300

    s

--- src/AnimalAdoption.BusinessLogic/Dtos/StatisticsDto.cs
+++ src/AnimalAdoption.BusinessLogic/Dtos/StatisticsDto.cs
@@ -6,12 +6,12 @@
         public int ActiveAdoptionAnnouncements { get; set; }
         public int ClosedAdoptionAnnouncements { get; set; }
         public int AdoptionRequestsNumber { get; set; }
-        public decimal AdoptionAverage { get; set; }
+        public float AdoptionAverage { get; set; }
         public int FosteringAnnouncementsCount { get; set; }
         public int ActiveFosteringAnnouncements { get; set; }
         public int ClosedFosteringAnnouncements { get; set; }
         public int FosteringRequestsNumber { get; set; }
-        public decimal FosteringAverage { get; set; }
+        public float FosteringAverage { get; set; }
         public int AdoptionUsersNo { get; set; }
         public int AcceptedAdoptionRequests { get; set; }
         public int RejectedAdoptionRequests { get; set; }
